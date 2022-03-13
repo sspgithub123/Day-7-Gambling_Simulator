118ea8b7dcbdc604bb6bbc82b034d1aade5d29e3
Shubham <ssp671995@gmail.com>
2022-03-13 15:38:18 +0530

    Added welcome message

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling Simulator Game");
+    }
+}
