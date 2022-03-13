e6f148b7cca094dce6fa935ec9fb6bb902115a33
Shubham <ssp671995@gmail.com>
2022-03-13 15:42:27 +0530

    Adding the start stake and bet stake.

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,8 +1,11 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+    public static int stack = 100;
+    public static int bet = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulator Game");
+        System.out.println(stack + " " + bet);
     }
 }
