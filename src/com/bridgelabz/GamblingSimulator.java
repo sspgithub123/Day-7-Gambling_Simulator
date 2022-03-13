8a3c1a0b4db10b93f75e0191827edd0f003f35b6
Shubham <ssp671995@gmail.com>
2022-03-13 15:46:05 +0530

    Adding the Win or Loose Condition.

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,11 +1,29 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class GamblingSimulator {
-    public static int stack = 100;
-    public static int bet = 1;
+    public static int STAKE = 100;
+    public static int BET = 1;
+    public static int Win = 0;
+    public static int Loose = 0;
+
+    public static void checkWinOrLoose() {
+        Random Number = new Random();
+        int randomNumber = Number.nextInt(2);
+        if (randomNumber == 0) {
+            Win = BET;
+            System.out.println("Gambler has won ");
+        } else {
+            Loose = BET;
+            System.out.println("Gamler has lost ");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambling Simulator Game");
-        System.out.println(stack + " " + bet);
+        System.out.println(STAKE + " " + BET);
+        System.out.println(" ");
+        checkWinOrLoose();
     }
 }
